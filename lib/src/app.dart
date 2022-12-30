7dac5ea61db3de44503211505fcb5eae1b6c6679
suarezKevin <suarezk453@gmail.com>
2022-12-29 20:29:10 -0500

    DBS-17 Cambio del nombre del campo 'Nombre de Usuario' por 'Email'

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       //home:
       initialRoute: "/",
       routes: {
